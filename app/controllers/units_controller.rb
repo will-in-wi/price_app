4418dae2960806656d080313e05a963481a926a2
William Johnston <wjohnston@mpr.org>
2014-08-24 22:39:31 -0500

    Fix unit/unit_category relationship

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -69,6 +69,6 @@ class UnitsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def unit_params
-      params.require(:unit).permit(:unit_category, :amount, :default)
+      params.require(:unit).permit(:unit_category_id, :name, :amount, :default)
     end
 end
