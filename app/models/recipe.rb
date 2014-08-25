3b77f5e4cfef12304aa5d60fa8f424b8baf98948
William Johnston <wjohnston@mpr.org>
2014-08-24 22:48:54 -0500

    Set up models for nested forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
+  has_many :items, through: :recipe_items
   has_many :recipe_items
+  accepts_nested_attributes_for :recipe_items
 end
--- app/models/recipe_item.rb
+++ app/models/recipe_item.rb
@@ -1,2 +1,4 @@
 class RecipeItem < ActiveRecord::Base
+  belongs_to :recipe
+  belongs_to :item
 end
