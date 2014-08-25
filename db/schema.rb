81d3705b0012e5c06436f08bc68c5ef802621884
William Johnston <wjohnston@mpr.org>
2014-08-24 22:36:15 -0500

    Add name to unit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824011826) do
+ActiveRecord::Schema.define(version: 20140825033601) do
 
   create_table "items", force: true do |t|
     t.integer  "unit_category_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140824011826) do
     t.boolean  "default"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
